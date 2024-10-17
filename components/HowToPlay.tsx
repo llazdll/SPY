4598306ce4f774a045b98f7907608c07f0e518dc
llazdll <102923017+llazdll@users.noreply.github.com>
2024-10-17 13:18:39 +0330

    Update HowToPlay.tsx to Persian

--- components/HowToPlay.tsx
+++ components/HowToPlay.tsx
@@ -9,19 +9,19 @@ import {
     return (
       <Accordion type="single" collapsible className="w-full">
         <AccordionItem value="item-1">
-          <AccordionTrigger>How to Play?</AccordionTrigger>
+          <AccordionTrigger>چه جوری بازی کنم</AccordionTrigger>
           <AccordionContent className="p-8 text-right">
           دو گروه وجود دارد جاسوس و شهروند بازی به این صورت است که به گروه شهروند یک کلمه نمایش داده می‌شود و به گروه جاسوس یک راهنمایی راجع به اون کلمه مایش داده می‌شود . <br/>گروه جاسوس باید کلمه مورد نظر را حدس بزنند و گروه شهروند باید جاسوس‌ها را پیدا کن
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-2">
-          <AccordionTrigger>Time?</AccordionTrigger>
+          <AccordionTrigger>Time? (زمان)</AccordionTrigger>
           <AccordionContent className="p-8 text-right">
             زمان بر حسب دقیقه است و پس از تمام شدن زمان, بازی تمام میشود و اگر شهروند نتواند جاسوس را پیدا کند بازی به نفع جاسوس تمام میشود
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-3">
-          <AccordionTrigger>Player?</AccordionTrigger>
+          <AccordionTrigger>Player?(بازیکن)</AccordionTrigger>
           <AccordionContent className="p-8 text-right">
             تعداد همه کسانی که بازی میکنند هم جاسوس هم شهروند
           </AccordionContent>
